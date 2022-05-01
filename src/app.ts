0988959e2b8de057a55bf765ac90a333acced9bc
WelGomws <wellingtondomingos.gomes@gmail.com>
2022-05-01 16:48:08 -0300

    feat: export app

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import { connection } from "./config/db";
+import Routes from "./routes";
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/dev", Routes);
+
+connection().then(() => {
+  const server = app.listen(process.env.PORT, () => {
+    console.log("server on!");
+  });
+
+  server.on("error", console.error);
+});
