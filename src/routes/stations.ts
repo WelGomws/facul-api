bb9f7b4aaee625e911ad9e42932de6f475aa2b76
WelGomws <wellingtondomingos.gomes@gmail.com>
2022-05-01 19:45:05 -0300

    feat: add authentication in routes

--- src/routes/stations.ts
+++ src/routes/stations.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { controllerStation } from "../controllers";
-
+import { authentication } from "../middlewares/authentication";
 const routes = Router();
 
-routes.post("/", controllerStation.create);
+routes.post("/", authentication, controllerStation.create);
 routes.get("/", controllerStation.get);
-routes.patch("/:id", controllerStation.update);
+routes.patch("/:id", authentication, controllerStation.update);
 
 export default routes;
