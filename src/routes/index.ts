0a88b41aff8c425bce1d6a0eea1a7cf1a50232ea
WelGomws <wellingtondomingos.gomes@gmail.com>
2022-05-01 19:45:26 -0300

    feat: export routes user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import healthRoutes from "./health";
 import stationRoutes from "./stations";
 import lineRoutes from "./line";
+import userRoutes from "./user";
 
 const routes = Router();
 
 routes.use("/health", healthRoutes);
 routes.use("/stations", stationRoutes);
 routes.use("/lines", lineRoutes);
+routes.use("/user", userRoutes);
 
 export default routes;
