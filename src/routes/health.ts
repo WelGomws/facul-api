b9e5a9fa6883b7f2ca747a2faf02917bba1c6a0e
WelGomws <wellingtondomingos.gomes@gmail.com>
2022-05-01 16:41:03 -0300

    feat: health route

---
+++ src/routes/health.ts
@@ -0,0 +1,9 @@
+import { Request, Response, Router } from "express";
+
+const routes = Router();
+
+routes.get("/", (request: Request, response: Response) => {
+  response.json({ ok: true, version: "0.0.1" });
+});
+
+export default routes;
