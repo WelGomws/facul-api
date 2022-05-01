b5c5187034397ee1852f2a08cccf5d1d2ca20eb4
WelGomws <wellingtondomingos.gomes@gmail.com>
2022-05-01 16:40:01 -0300

    feat: create model line

---
+++ src/models/line.ts
@@ -0,0 +1,13 @@
+import { model, Schema } from "mongoose";
+
+const lineSchema = new Schema(
+  {
+    name: { type: String, required: true, unique: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const lineModel = model("Lines", lineSchema);
