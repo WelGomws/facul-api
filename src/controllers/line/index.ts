5ec725945b51953173a76f0bc10bea83d59004f5
WelGomws <wellingtondomingos.gomes@gmail.com>
2022-05-13 14:51:35 -0300

    feat: export new controller

--- src/controllers/line/index.ts
+++ src/controllers/line/index.ts
@@ -1,7 +1,9 @@
 import { create } from "./create";
 import { get } from "./get";
+import { getWithStations } from "./get-with-stations";
 
 export const controllerLine = {
   create,
   get,
+  getWithStations,
 };
