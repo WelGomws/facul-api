f3aa443d89b528861e6c73e6bdc372a4dbd7b22a
WelGomws <wellingtondomingos.gomes@gmail.com>
2022-05-14 09:08:19 -0300

    refac: change properties in comment model

--- src/models/comment.ts
+++ src/models/comment.ts
@@ -4,11 +4,12 @@ const commentSchema = new Schema(
   {
     author: { type: String, required: false },
     comment: { type: String, required: true },
-    active: { type: Boolean, required: true },
-    station: { type: Schema.Types.ObjectId, ref: "Stations" },
+    active: { type: Boolean, required: true, default: true },
+    station: { type: Schema.Types.ObjectId, ref: "Stations", required: false },
   },
   {
     timestamps: true,
+    versionKey: false,
   }
 );
 
