d6c86d4375d8f12e5d2f9f763a4a149b9af07a8c
WelGomws <wellingtondomingos.gomes@gmail.com>
2022-05-14 09:09:23 -0300

    feat: export route comments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import healthRoutes from "./health";
 import stationRoutes from "./stations";
 import lineRoutes from "./line";
 import userRoutes from "./user";
+import commentsRoutes from "./comments";
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use("/health", healthRoutes);
 routes.use("/stations", stationRoutes);
 routes.use("/lines", lineRoutes);
 routes.use("/user", userRoutes);
+routes.use("/comments", commentsRoutes);
 
 export default routes;
