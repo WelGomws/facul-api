9bfa205bb74269e09bc339c428575da395700e5a
WelGomws <wellingtondomingos.gomes@gmail.com>
2022-05-22 19:31:30 -0300

    feat: new route for test token valid

--- src/routes/line.ts
+++ src/routes/line.ts
@@ -7,5 +7,6 @@ const routes = Router();
 routes.post("/", authentication, controllerLine.create);
 routes.get("/", controllerLine.get);
 routes.get("/with-stations", controllerLine.getWithStations);
+routes.get("/authenticate", authentication, controllerLine.getWithStations);
 
 export default routes;
